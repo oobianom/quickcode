1742030e9ce5683b8d29626ddf578a543785f6d3
Obi Obianom <oobianom@umaryland.edu>
2023-08-02 11:29:34 -0400

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -614,7 +614,17 @@ header.rmd <- function() {
 
 Document Path: ", rstudioapi::getActiveDocumentContext()$path, "
 
-Written by USERNAME on ", Sys.Date(), "
+Author:
+
+Date: ", Sys.Date(), "
+
+Title:
+
+Description:
+
+Required Files:
+
+Exported Files:
 
 R Version: ", version$version.string, "
 
