3f601ef47490098bc8226d077359151b8e561745
Obi Obianom <oobianom@umaryland.edu>
2023-08-08 19:17:17 -0400

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -115,6 +115,25 @@ clean <- function(setwd = NULL, source = c(), load = c(), clearPkgs = FALSE) {
 
 not.numeric <- function(x) !is.numeric(x)
 
+#' Not duplicated elements
+#'
+#' Checks which elements of a vector or data frame are NOT duplicates of elements with smaller subscripts
+#'
+#' @param x a vector or a data frame or an array or NULL.
+#' @param incomparables a vector of values that cannot be compared. FALSE is a special value, meaning that all values can be compared, and may be the only value accepted for methods other than the default. It will be coerced internally to the same type as x
+#' @param ... arguments for particular methods.
+#' @return elements of a vector or data frame that are NOT duplicates
+#' @examples
+#'
+#' dtf <- sample(1:10,15, replace = TRUE)
+#' dtf[not.duplicated(dtf)]
+#'
+#' @export
+
+not.duplicated <- function(x, incomparables = FALSE, ...)
+  !duplicated(x = x, incomparables = incomparables, ...)
+
+
 
 #' Not NULL
 #'
