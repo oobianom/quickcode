4433ec36d1870d3f6d47113a7f5229562d966114
Obi Obianom <oobianom@umaryland.edu>
2023-08-13 11:47:43 -0400

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -563,18 +563,19 @@ data_pop <- function(., n = 1, which = c("rows", "cols"), ret = FALSE) {
 #' @param remove expression for filter
 #' @return data filtered out based on the expression
 #' @examples
-#'
+#' # this function removes rows matching the filter expression
 #' data.01 <- mtcars
+#' data.02 <- airquality
 #'
 #' #task: remove all mpg > 20
 #' data.01 #data.01 data before pop
 #' data_pop_filter(data.01,mpg > 15) #computes and resaves to variable
 #' data.01 #modified data after pop based on filter
 #'
-#' #task: remove all multiple. remove all elements where carb  == 4 or hp > 200
-#' data.01 #data.01 data before pop
-#' data_pop_filter(data.01,carb  == 4 | hp > 200 ) #computes and resaves to variable
-#' data.01 #modified data after pop based on filter
+#' #task: remove all multiple. remove all elements where Month   == 5 or Solar.R > 50
+#' data.02 #data.02 data before pop
+#' data_pop_filter(data.02,Month   == 5 | Solar.R > 50) #computes and resaves to variable
+#' data.02 #modified data after pop based on filter
 #'
 #' @export
 #'
