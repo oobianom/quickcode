bc43dcab6bb001bb08a7258d2ccbef9cf7b17c41
Obi Obianom <oobianom@umaryland.edu>
2023-08-29 00:40:41 -0400

    Update sd.gm.R

--- R/sd.gm.R
+++ R/sd.gm.R
@@ -4,6 +4,7 @@
 #'
 #' @param num vector of numbers
 #' @param na.rm remove NAs from the vector
+#' @param neg.rm remove negative values from the vector
 #' @param round round result to decimal place
 #' @return the geometric standard deviation of a set of numbers
 #' @examples
@@ -12,7 +13,14 @@
 #'
 #' @export
 
-sd.gm <- function(num, na.rm = TRUE, round = 2) {
+sd.gm <- function(num, na.rm = TRUE, neg.rm = TRUE, round = 2) {
   if(not.numeric(num)) stop("The vector must have numbers only")
-  return(round(exp(stats::sd(log(num[num > 0]), na.rm = na.rm)),round))
+  if(neg.rm) num <- num[num > 0]
+  return(round(exp(stats::sd(log(num), na.rm = na.rm)),round))
 }
+
+
+#' @inherit sd.gm
+#' @export
+
+sd.gm -> geo.sd
