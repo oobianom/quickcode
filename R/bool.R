39911b4e959c67fd96980608adca5192f4fe1ac0
Obi Obianom <oobianom@umaryland.edu>
2023-09-18 00:06:36 -0400

    Update bool.R

--- R/bool.R
+++ R/bool.R
@@ -5,14 +5,19 @@
 bool <- function(., conv.to = c(1, TRUE, "Yes")) {
   # match conversion to
   conv.to <- match.arg(conv.to)
+  .lower <- tolower(.)
+
+  .bin1 <- ifelse(grep("^((yes)|(true)|(1))$",.),1,
+         ifelse(grep("^((no)|(false)|(0))$",.),0,NA))
 
   # standardize to binary
-  switch(tolower(.),
-    "true" = 1,
-    "yes" = 1,
-    "no" = 0,
-    "false" = 0
-  )
+  # switch(tolower(.),
+  #   "true" = 1,
+  #   "yes" = 1,
+  #   "no" = 0,
+  #   "false" = 0
+  # )
 
   # return based on conv.to
+  .bin1
 }
