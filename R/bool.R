49cde9590201bf9e9d70a06a6415cc0fc059a389
Obi Obianom <oobianom@umaryland.edu>
2023-09-17 20:55:11 -0400

    Update bool.R

--- R/bool.R
+++ R/bool.R
@@ -2,6 +2,17 @@
 #'
 #' @export
 #'
-bool <- function(.,conv.to = c(1,TRUE,'Yes')){
+bool <- function(., conv.to = c(1, TRUE, "Yes")) {
+  # match conversion to
+  conv.to <- match.arg(conv.to)
 
+  # standardize to binary
+  switch(tolower(.),
+    "true" = 1,
+    "yes" = 1,
+    "no" = 0,
+    "false" = 0
+  )
+
+  # return based on conv.to
 }
