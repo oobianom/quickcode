52410051c3a0c6733fd7a5909a310fc626adf124
Obi Obianom <oobianom@umaryland.edu>
2023-09-18 00:30:00 -0400

    Update bool.R

--- R/bool.R
+++ R/bool.R
@@ -5,14 +5,13 @@
 #'
 #' @export
 #'
-as.boolean <- function(., conv.to = 1:3) {
+as.boolean <- function(., conv.to = "1":"3") {
   # match conversion to
   conv.to <- match.arg(conv.to)
   print(conv.to)
-  .lower <- tolower(.)
   # standardize to binary
-  .res <- switch(.lower,"true" = 1,"t" = 1,"yes" = 1,"y" = 1,"no" = 0, "n" = 0, "false" = 0, "f" = 0 )
-  .res <- switch(.res,"1" = 1,"2" = 1,"3" = 1)
+  .. <- switch(tolower(.),"true" = 1,"t" = 1,"yes" = 1,"y" = 1,"no" = 0, "n" = 0, "false" = 0, "f" = 0 )
+  ifelse(..,switch(..,"1" = "Yes","2" = TRUE,"3" = 1),switch(..,"1" = "No","2" = FALSE,"3" = 0))
 
   # return based on conv.to
 }
