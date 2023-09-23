d00cecaad34923cab059641e7d7f4ea034be0fc3
Obi Obianom <oobianom@umaryland.edu>
2023-09-23 13:19:02 -0400

    Update randomImage.R

--- R/randomImage.R
+++ R/randomImage.R
@@ -21,29 +21,44 @@
 #'
 #'
 #' @examples
-#' # download 1 image from the nature category
-#' genImage(fp="~/",cat = "nature", n = 1)
+#' # download 10 image from the nature category
+#' genImage(fp="D:/GithubRepo/samples/randomimages",cat = "nature", n = 20)
 #'
 #'
 #'
 #' @export
 #'
-genImage <- function(fp = "~/images", cat = "nature", n = 1, w.px = 500, h.px = 500, ext = "jpg"){
+genImage <- function(fp = "~/images", cat = "nature", n = 1, w.px = 500, h.px = 500, ext = "jpg") {
   checksum.files <- c()
-  while(n > 0) {
-    temp.store <- paste0(tempfile(),".",ext)
+  temp.files <- c()
+
+  # download files to temp directory
+  while (n > 0) {
+    temp.store <- tempfile(fileext = paste0(".",ext))
     download.file(
-      url = paste0("https://source.unsplash.com/random/",w.px,"x",h.px,"/?",cat,"&", i),
-      destfile = temp.store, mode = 'wb')
+      url = paste0("https://source.unsplash.com/random/", w.px, "x", h.px, "/?", cat, "&", i),
+      destfile = temp.store, mode = "wb"
+    )
+
+    # make sure there is no duplication
     checksum <- tools::md5sum(temp.store)
-    if(checksum %in% checksum.files){
+    if (checksum %in% checksum.files) {
       unlink(temp.store)
       plus(n)
-    }else{
-      vector_push(checksum.files,checksum)
+      print(checksum)
+    } else {
+      vector_push(checksum.files, checksum)
+      vector_push(temp.files, temp.store)
       minus(n)
     }
-    print(temp.store)
   }
-
+  # move files
+  message("Moving downloaded files to specified folder...")
+  names(temp.files) <- temp.files
+  if (length(temp.files)) {
+    lapply(temp.files, function(i) {
+      file.rename(i, file.path(fp, basename(i)))
+    })
+  }
+  message(paste0("Downloaded ",length(temp.files)," files to ",fp))
 }
