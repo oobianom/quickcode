14c9c458e2e3acf42d7541f9e1df8ba7fbc5ca95
Obi Obianom <oobianom@umaryland.edu>
2023-09-23 12:05:58 -0400

    Update vector_push

--- R/vector_push.R
+++ R/vector_push.R
@@ -5,6 +5,8 @@
 #' @param . first vector
 #' @param add vector to add
 #' @param unique remove duplicated entries
+#' @param rm.na remove NA values
+#' @param rm.null remove NULL values
 #' @return vector combining fist and second vector, but have name set to the first
 #' @examples
 #' num1 <- number(10)
@@ -20,24 +22,49 @@
 #'
 #'
 #' #Task: concatenate two vectors and remove duplicates
-#' vector1 = 1:10
-#' vector2 = 5:20
-#' vector3 = 15:25
+#' vector1 = number(4,seed = 5)
+#' vector2 = number(8,seed = 5)
+#' vector3 = number(12,seed = 5)
+#'
+#' vector1 #length is 4
+#' vector2 #length is 8
+#' vector3 #length is 12
 #'
 #' # with duplicated
 #' vector_push(vector1,vector2, unique = FALSE)
 #' vector1 #return modified vector
+#' length(vector1) #length is 12 because nothing was removed
+#' #duplicates in vector1 is 886905927 100040083 293768998  54080431
 #'
 #' # without duplicated
 #' vector_push(vector2,vector3, unique = TRUE)
 #' vector2 #return modified vector
+#' length(vector2) #length is 12 instead of 20
+#' #Total of 8 duplicated numbers were removed
+#'
+#'
+#' #Task: concatenate two vector and remove NA values
+#' vector1 = number(5)
+#' vector2 = c(4,NA,5,NA)
+#' vector3 = number(5)
+#'
+#' # with NA
+#' vector_push(vector1,vector2, rm.na = FALSE)
+#' vector1 #return modified vector
+#'
+#' # without NA
+#' vector_push(vector3,vector2, rm.na = TRUE)
+#' vector3 #return modified vector
+#'
 #' @export
 #'
-vector_push <- function(., add, unique = FALSE) {
+vector_push <- function(., add, unique = FALSE, rm.na = FALSE, rm.null = FALSE) {
   .. <- substitute(.)
   if (typeof(..) != "symbol") stop(paste0(.., " must be an object."))
   .res <- c(get(as.character(..), envir = parent.frame()), add)
   if(unique) .res <- unique(.res)
+  if(rm.na) .res <- .res[not.na(.res)]
+  if(rm.null) .res <- .res[not.null(.res)]
   assign(as.character(..), .res , envir = parent.frame())
 }
 
