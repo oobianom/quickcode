0e0b19fe4989a227bef8633fed46407be57d45d6
Obi Obianom <oobianom@umaryland.edu>
2023-09-23 11:47:45 -0400

    Update vector_push.R

--- R/vector_push.R
+++ R/vector_push.R
@@ -4,6 +4,7 @@
 #'
 #' @param . first vector
 #' @param add vector to add
+#' @param unique remove duplicated entries
 #' @return vector combining fist and second vector, but have name set to the first
 #' @examples
 #' num1 <- sample(330:400,10)
@@ -11,17 +12,32 @@
 #' num2 <-"rpkg.net"
 #'
 #'
-#' #seamlessly add num2 to num1 and re-save as num1
+#' #Task: add num2 to num1 and re-save as num1
 #'
 #' vector_push(num1,num2)
 #' num1 #updated with num2
 #' num2 #not updated
 #'
+#'
+#' #Task: concatenate two vectors and remove duplicates
+#' vector1 = 1:10
+#' vector2 = 5:20
+#' vector3 = 15:25
+#'
+#' # with duplicated
+#' vector_push(vector1,vector2, unique = FALSE)
+#' vector1 #return modified vector
+#'
+#' # without duplicated
+#' vector_push(vector2,vector3, unique = TRUE)
+#' vector2 #return modified vector
 #' @export
 #'
-vector_push <- function(., add) {
+vector_push <- function(., add, unique = FALSE) {
   .. <- substitute(.)
   if (typeof(..) != "symbol") stop(paste0(.., " must be an object."))
-  assign(as.character(..), c(get(as.character(..), envir = parent.frame()), add), envir = parent.frame())
+  .res <- c(get(as.character(..), envir = parent.frame()), add)
+  if(unique) .res <- unique(.res)
+  assign(as.character(..), .res , envir = parent.frame())
 }
 
