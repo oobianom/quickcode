60ba1a0e8766d13d6d84fe7d029ca9387c8a102e
Obi Obianom <oobianom@umaryland.edu>
2023-09-24 11:42:45 -0400

    Update aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,9 +1,7 @@
 # Internal functions and miscellaneous
-clean()
 # random string
 randString <- function(n, length) {
   .combo <- c(0:9, letters, 0:9, LETTERS)
   .all <- matrix(sample(.combo, n * length, replace = TRUE), ncol = n)
   unlist(lapply(1:ncol(.all), function(i)paste(.all[, i], collapse = "")))
 }
-
