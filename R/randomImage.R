d15705cd762fb2ce1c6a1013dc5c304ca7bb9b2a
Obi Obianom <oobianom@umaryland.edu>
2023-09-23 20:01:34 -0400

    Update randomImage.R

--- R/randomImage.R
+++ R/randomImage.R
@@ -4,7 +4,7 @@
 #'
 #' @param fp Storage directory
 #' @param cat category of image to download
-#' @param n number of images to download
+#' @param n number of images to download, maximum n is 99
 #' @param w.px width in pixels
 #' @param h.px height in pixels
 #' @param ext file extension eg jpg, png
@@ -21,15 +21,21 @@
 #'
 #' @examples
 #' # download 2 image from the nature category
-#' genImage(fp="~/",cat = "nature", n = 2)
+#' genImage(fp=tempdir(),cat = "nature", n = 2)
 #'
 #'
 #'
 #' @export
 #'
-genImage <- function(fp,cat, n = 1, w.px = 500, h.px = 500, ext = "jpg") {
+genImage <- function(n = 1, cat, fp = tempdir(), w.px = 500, h.px = 500, ext = "jpg") {
+  # check that n does not exceed 99
+  if(n > 99){
+    warning("The value of n exceeds 99, so n has been automatically set to 99")
+    n <- 99
+  }
+  # check sum and file names
   checksum.files <- c()
-  temp.files <- c()
+  downloaded.files <- c()
 
   # download files to temp directory
   while (n > 0) {
@@ -43,12 +49,11 @@ genImage <- function(fp,cat, n = 1, w.px = 500, h.px = 500, ext = "jpg") {
     checksum <- as.vector(tools::md5sum(file.store))
     if (checksum %in% checksum.files) {
       unlink(file.store)
-      plus(n)
     } else {
       vector_push(checksum.files, checksum)
-      vector_push(temp.files, file.store)
+      vector_push(downloaded.files, file.store)
       minus(n)
     }
   }
-  message(paste0("Downloaded ",length(temp.files)," files to ",fp))
+  message(paste0("Downloaded ",length(downloaded.files)," files to ",fp))
 }
