3acf2fc219543d00f03bb055cb4d1081b1156020
Obi Obianom <oobianom@umaryland.edu>
2023-09-23 20:59:36 -0400

    Update rcolorconst.R

--- R/rcolorconst.R
+++ R/rcolorconst.R
@@ -1,9 +1,29 @@
 #' R Color Constant
 #'
+#' @param title title of the output
+#'
 #' @export
 
-rcolorconst <- function(){
-  rcolorconst = c("black" = 1, "red" = 2, "green" = 3, "blue" = 4, "cyan" = 5, "magenta" = 6, "orange" = 7, "gray" = 8)
-  rcolconst = c("black", "red", "green", "blue", "cyan", "magenta", "orange", "gray")
-  Polychrome::swatch(rcolconst, main = "R Color Constants")
+rcolorconst <- function(title = "R Color Constants"){
+  rcolorconst = c(
+    "black" = "#000000",
+    "red" = "#FF0000",
+    "green" = "#00FF00",
+    "blue" = "#0000FF",
+    "cyan" = "#00FFFF",
+    "magenta" = "#FF00FF",
+    "orange" = "#FFA500",
+    "gray" = "#BEBEBE")
+  luvmat <<- as(colorspace::hex2RGB(rcolorconst), "LUV")
+  print(luvmat)
+  x <- luvmat@coords
+  labelcols <- c("white", "black")[1 + 1 * (x[,1] > 50)]
+  L <- length(rcolorconst)
+  pts <- barplot(rep(1, L), col = rcolorconst, main = title, yaxt = "n")
+  text(pts, 0.5, names(rcolorconst), srt = 90, col = labelcols)
+  invisible(pts)
 }
+
+
+
+
