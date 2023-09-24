69102757895ab7c52afe1132b4b4726ab1c0a412
Obi Obianom <oobianom@umaryland.edu>
2023-09-24 11:20:02 -0400

    Update description file to include Brice

--- R/aaa.R
+++ R/aaa.R
@@ -1,11 +1,11 @@
 # Internal functions and miscellaneous
 
 # random string
-randString <- function(n,length){
+randString <- function(n, length) {
   .ret <- c()
-  .combo <- c(0:9,letters,0:9,LETTERS)
-  .all <- matrix(sample(.combo,n * length, replace = TRUE), ncol = n)
-  for(i in 1:ncol(.all)) vector_push(.ret,paste(.all[,i], collapse = ""))
+  .combo <- c(0:9, letters, 0:9, LETTERS)
+  .all <- matrix(sample(.combo, n * length, replace = TRUE), ncol = n)
+  for (i in 1:ncol(.all)) vector_push(.ret, paste(.all[, i], collapse = ""))
   .ret
 }
 
