b646ea9e11f306fe01aba010bdffaf35a2243e45
Obi Obianom <oobianom@umaryland.edu>
2023-09-24 17:16:47 -0400

    Update archived R packages retrieval

--- R/archivedR.R
+++ R/archivedR.R
@@ -2,12 +2,23 @@
 #'
 #' Retrieve a list of all currently archived R packages
 #'
+#' @param n number of packages to retrieve
 #'
+#' @return a data frame container listing of all archived R packages
+#'
+#' @note
+#' If the n argument is set to 0, all packages will be retrieved. Keep in mind that the larger the count or if all packages
+#' are requested, this function becomes slower.
 #'
 #'
 #' @export
 #'
 
-archpkg  <- function(n = NULL){
-
+archivedPkg <- function(n = 0) {
+  read.csv(
+    file = paste0(
+      "https://quickcode.obi.obianom.com/CRAN/searchArchivedCRANPkg.php?count=",
+      as.integer(n)),
+    header = TRUE,
+    skip = 1)
 }
