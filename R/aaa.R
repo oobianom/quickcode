2bf2ba43abe7b9fba4b4133f495a6ef1b4144832
Obi Obianom <oobianom@umaryland.edu>
2023-09-25 23:32:53 -0400

    Update aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -10,5 +10,11 @@ randString <- function(n, length) {
 # check date format
 # expected format  YYYY-MM-DD
 check_date_format <- function(date){
-
+  splitdate <- strsplit(date,"-")[[1]]
+  if(length(splitdate) != 3) stop("Date format must be YYYY-MM-DD")
+  if(nchar(splitdate[1]) != 4) stop("Year format must be YYYY e.g 2010")
+  if(nchar(splitdate[2]) != 2) stop("Month format must be MM e.g 05")
+  if(nchar(splitdate[3]) != 2) stop("Day format must be DD e.g 25")
+  if(as.numeric(splitdate[2]) > 12 | as.numeric(splitdate[2]) < 1) stop("Month format must be between 01 and 12")
+  if(as.numeric(splitdate[3]) > 31 | as.numeric(splitdate[3]) < 1) stop("Day format must be between 01 and 12")
 }
