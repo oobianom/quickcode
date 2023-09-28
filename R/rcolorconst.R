5be935dced308e325337220168148d59b7789fc6
Obi Obianom <oobianom@umaryland.edu>
2023-09-28 15:40:18 -0400

    Update rcolorconst.R

--- R/rcolorconst.R
+++ R/rcolorconst.R
@@ -12,14 +12,14 @@
 #' @export
 
 rcolorconst <- function(title = "R Color Constants"){
-  rcolorconst = c(1 ="black",
-                  2 ="red",
-                  3 = "green",
-                  4 = "blue",
-                  5 = "cyan",
-                  6 = "magenta",
-                  7 = "orange",
-                  8 = "gray")
+  rcolorconst = c("1" ="black",
+                  "2" ="red",
+                  "3" = "green",
+                  "4" = "blue",
+                  "5" = "cyan",
+                  "6" = "magenta",
+                  "7" = "orange",
+                  "8" = "gray")
   Polychrome::swatch(rcolorconst, main = title)
 }
 
