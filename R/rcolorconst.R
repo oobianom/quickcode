ea945b72c89e19ddb9ceed32820f60af07edfc1b
Obi Obianom <oobianom@umaryland.edu>
2023-09-28 23:22:25 -0400

    Update rcolorconst.R

--- R/rcolorconst.R
+++ R/rcolorconst.R
@@ -33,20 +33,3 @@ rcolorconst <- function(title = "R Color Constants"){
   setNames(as.numeric(names(rcolorconst)), rcolorconst)
 }
 
-
- # rcolorconst = c(
- #    "black" = "#000000",
- #    "red" = "#FF0000",
- #    "green" = "#00FF00",
- #    "blue" = "#0000FF",
- #    "cyan" = "#00FFFF",
- #    "magenta" = "#FF00FF",
- #    "orange" = "#FFA500",
- #    "gray" = "#BEBEBE")
- #
- #  labelcols <- c("white", "black")[c(1,2,2,1,2,2,2,2)]
- #  L <- length(rcolorconst)
- #  pts <- graphics::barplot(rep(1, L), col = rcolorconst, main = title, yaxt = "n")
- #  graphics::text(pts, 0.5, names(rcolorconst), srt = 90, col = labelcols)
- #  invisible(pts)
-
