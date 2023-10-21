91f6ac6c066a2dc8de0adc6179e101a4373e01aa
Obi Obianom <oobianom@umaryland.edu>
2023-10-21 19:58:21 -0400

    Update plots.R

--- R/plots.R
+++ R/plots.R
@@ -14,4 +14,25 @@
 
 compHist <- function(x1, x2, main = "Histogram of rnorm Distributions With Means 0 & 2"){
 message("Function is still underdevelopment. Please do not use.")
+
+  set.seed(249)
+  x1 = rnorm(1000, mean = 0)
+  x2 = rnorm(1000, mean = 2)
+
+  hist(x1,
+       main = main,
+       xlab = "",
+       ylab = "",
+       col = rgb(0, 0, 1, alpha = 0.6),
+       xlim = c(minx, maxx))
+
+  hist(x2, xlab = "",
+       ylab = "",
+       col = rgb(1, 0, 0, alpha = 0.6),
+       add = TRUE)
+
+  legend("topright",
+         legend = c("Mean: 0", "Mean: 2", "Overlap"),
+         fill = c("lightslateblue", "salmon","mediumvioletred"))
+
 }
