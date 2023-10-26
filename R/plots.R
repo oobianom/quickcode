a958aedbf580f6e778b10a635e70da673bc30aa5
Obi Obianom <oobianom@umaryland.edu>
2023-10-26 06:44:10 -0400

    Update plots.R

--- R/plots.R
+++ R/plots.R
@@ -42,7 +42,7 @@ compHist <- function(x1, x2, title, color = c("green", "black", "yellow"), xlab 
   maxx <- max(x1x2) + 0.1 * max(x1x2)
 
   # close devices if open
-  dev.off()
+  if(not.null(.Device))dev.off()
 
   # check if plots should be separated
   if (separate) par(mfrow = c(1, 2))
