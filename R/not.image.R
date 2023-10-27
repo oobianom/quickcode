e3adc8d4976c0f9574f8aa38dd6077c752e11239
Obi Obianom <oobianom@umaryland.edu>
2023-10-27 18:50:41 -0400

    Update not.image.R

--- R/not.image.R
+++ R/not.image.R
@@ -4,6 +4,43 @@
 #'
 #' @param x vector entry
 #' @return a boolean value to indicate if entry is not an image
+#'
+#' @details
+#' This current function tests if the extension of file name provided belongs to any of the image extensions listed below \cr
+#' AI - Adobe Illustrator \cr
+#' BMP - Bitmap Image \cr
+#' CDR - Corel Draw Picture \cr
+#' CGM - Computer Graphics Metafile \cr
+#' CR2 - Canon Raw Version 2 \cr
+#' CRW - Canon Raw \cr
+#' CUR - Cursor Image \cr
+#' DNG - Digital Negative \cr
+#' EPS - Encapsulated PostScript \cr
+#' FPX - FlashPix \cr
+#' GIF - Graphics Interchange Format \cr
+#' HEIC - High-Efficiency Image File Format \cr
+#' HEIF - High-Efficiency Image File Format \cr
+#' ICO - Icon Image \cr
+#' IMG - GEM Raster Graphics \cr
+#' JFIF - JPEG File Interchange Format \cr
+#' JPEG - Joint Photographic Experts Group \cr
+#' JPG - Joint Photographic Experts Group \cr
+#' MAC - MacPaint Image \cr
+#' NEF - Nikon Electronic Format \cr
+#' ORF - Olympus Raw Format \cr
+#' PCD - Photo CD \cr
+#' PCX - Paintbrush Bitmap Image \cr
+#' PNG - Portable Network Graphics \cr
+#' PSD - Adobe Photoshop Document \cr
+#' SR2 - Sony Raw Version 2 \cr
+#' SVG - Scalable Vector Graphics \cr
+#' TIF - Tagged Image File \cr
+#' TIFF - Tagged Image File Format \cr
+#' WebP - Web Picture Format \cr
+#' WMF - Windows Metafile \cr
+#' WPG - WordPerfect Graphics
+#'
+#'
 #' @examples
 #' img.1 = "fjk.jpg"
 #' not.image(img.1)
@@ -32,6 +69,43 @@ not.image <- function(x) !is.image(x)
 #'
 #' @param x vector entry
 #' @return a boolean value to indicate if entry is an image
+#'
+#' @details
+#' This current function tests if the extension of file name provided belongs to any of the image extensions listed below \cr
+#' AI - Adobe Illustrator \cr
+#' BMP - Bitmap Image \cr
+#' CDR - Corel Draw Picture \cr
+#' CGM - Computer Graphics Metafile \cr
+#' CR2 - Canon Raw Version 2 \cr
+#' CRW - Canon Raw \cr
+#' CUR - Cursor Image \cr
+#' DNG - Digital Negative \cr
+#' EPS - Encapsulated PostScript \cr
+#' FPX - FlashPix \cr
+#' GIF - Graphics Interchange Format \cr
+#' HEIC - High-Efficiency Image File Format \cr
+#' HEIF - High-Efficiency Image File Format \cr
+#' ICO - Icon Image \cr
+#' IMG - GEM Raster Graphics \cr
+#' JFIF - JPEG File Interchange Format \cr
+#' JPEG - Joint Photographic Experts Group \cr
+#' JPG - Joint Photographic Experts Group \cr
+#' MAC - MacPaint Image \cr
+#' NEF - Nikon Electronic Format \cr
+#' ORF - Olympus Raw Format \cr
+#' PCD - Photo CD \cr
+#' PCX - Paintbrush Bitmap Image \cr
+#' PNG - Portable Network Graphics \cr
+#' PSD - Adobe Photoshop Document \cr
+#' SR2 - Sony Raw Version 2 \cr
+#' SVG - Scalable Vector Graphics \cr
+#' TIF - Tagged Image File \cr
+#' TIFF - Tagged Image File Format \cr
+#' WebP - Web Picture Format \cr
+#' WMF - Windows Metafile \cr
+#' WPG - WordPerfect Graphics
+#'
+#'
 #' @examples
 #' img.1 = "fjk.jpg"
 #' is.image(img.1)
@@ -55,7 +129,7 @@ not.image <- function(x) !is.image(x)
 
 is.image <- function(x){
   exts <- tools::file_ext(tolower(x))
-  unlist(lapply(exts, function(ext) ext %in% c('jpeg','jpg','jfif','png','gif','tiff','tif','bmp','svg','ico','psd','ai','eps','webp','heic','heif','nef','cr2','orf','dng','crw','sr2')))
+  unlist(lapply(exts, function(ext) ext %in% c("ai","bmp","cdr","cgm","cr2","crw","cur","dng","eps","fpx","gif","heic","heif","ico","img","jfif","jpeg","jpg","mac","nef","orf","pcd","pcx","png","psd","sr2","svg","tif","tiff","webp","wmf","wpg")  ))
 }
 
 
