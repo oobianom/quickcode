97a54fad50b992612ab38dfc476d5dc026a0125a
Obi Obianom <oobianom@umaryland.edu>
2023-11-12 02:11:40 -0500

    Update once.R

--- R/once.R
+++ R/once.R
@@ -1,6 +1,32 @@
-#' Set a variable once
+#' Set a variable only once
 #'
-#' Only set a variable once, where multiple calls are ignored
+#' Facilitates the one-time setting of a variable in R, ensuring its immutability thereafter.
+#'
+#' @param . variable to set
+#' @param val the value to set for the variable
+#'
+#' @return the variable set to the new variable, along with a class of once added to the output
+#'
+#' @details
+#' With this function, users can establish the change to the initial value of a variable,
+#' and it guarantees that any subsequent attempts to modify the variable are ignored.
+#' This feature ensures that the variable remains constant and immutable once it has been set,
+#' preventing unintentional changes and promoting code stability. This function simplifies the process
+#' of managing immutable variables in R, providing a reliable mechanism for enforcing
+#' consistency in data throughout the course of a program or script.
+#'
+#' @examples
+#' # set the value of vector_x1, vector_y1, vector_z1
+#' init(vector_x1, vector_y1, vector_z1, value = 85)
+#'
+#' # view the initial values of the variables
+#' vector_x1
+#' vector_y1
+#' vector_z1
+#'
+#' # task 1: change the value vector_x1 and prevent further changes
+#'
+#'
 #' @export
 
 setOnce <- function(., val = 1L) {
