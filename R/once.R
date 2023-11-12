b18fdacfc63f53b1f600d3efd3f784ef2bc517e3
Obi Obianom <oobianom@umaryland.edu>
2023-11-12 02:19:21 -0500

    Update once.R

--- R/once.R
+++ R/once.R
@@ -25,6 +25,36 @@
 #' vector_z1
 #'
 #' # task 1: change the value vector_x1 and prevent further changes
+#' vector_x1 # check value of unchanged
+#' vector_x1 * 0.56 # check value when x 0.56
+#'
+#' setOnce(vector_x1, val = 400) # set vector_x1
+#' vector_x1 # check value
+#' vector_x1 * 0.56 # check value when x 0.56
+#'
+#' setOnce(vector_x1, val = 400) # set vector_x1 AGAIN, should not change
+#' vector_x1 # check value
+#' vector_x1 * 0.56 # check value when x 0.56
+#'
+#' # task 2: In for loop, change vector_y1 and use later
+#' vector_y1 # check value of unchanged
+#'
+#' for(i in 1:20){
+#' setOnce(vector_y1,as.numeric(Sys.time()))
+#' # now let's see the difference between vector_y1
+#' # and the current time as it changes
+#' message(as.numeric(Sys.time()) - vector_y1)
+#' }
+#'
+#' # task 3: In for lapply, change vector_y1 and use later
+#' vector_y1 # check value of unchanged
+#'
+#' lapply(1:20, function(i){
+#' setOnce(vector_y1,as.numeric(Sys.time()))
+#' # now let's see the difference between vector_y1
+#' # and the current time as it changes
+#' message(as.numeric(Sys.time()) - vector_y1)
+#' })
 #'
 #'
 #' @export
