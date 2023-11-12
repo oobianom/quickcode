4c5ce6e551b0325ac833a8c7be530415d61b4b0f
Obi Obianom <oobianom@umaryland.edu>
2023-11-12 02:48:42 -0500

    Fix check results

--- R/plots.R
+++ R/plots.R
@@ -50,36 +50,36 @@ compHist <- function(x1, x2, title, color = c("green", "black", "yellow"), xlab 
   maxx <- max(x1x2) + 0.1 * max(x1x2)
 
   # close devices if open
-  if(.Device !="null device")dev.off()
+  if(.Device !="null device") grDevices::dev.off()
 
   # check if plots should be separated
-  if (separate) par(mfrow = c(1, 2))
+  if (separate) graphics::par(mfrow = c(1, 2))
   if (separate & length(title) != 2) {
     stop("Title must contain two titles if the plots are to be separated")
   }
 
 
   # make plots
-  hist(x1,
+  graphics::hist(x1,
     main = ifelse(separate, title[1], title),
     xlab = xlab,
     ylab = ylab,
-    col = rgb(0, 0, 1, alpha = 0.6),
+    col = grDevices::rgb(0, 0, 1, alpha = 0.6),
     xlim = c(minx, maxx)
   )
 
-  hist(x2,
+  graphics::hist(x2,
     main = ifelse(separate, title[2], title),
     xlab = xlab,
     ylab = ylab,
-    col = rgb(1, 0, 0, alpha = 0.6),
+    col = grDevices::rgb(1, 0, 0, alpha = 0.6),
     xlim = c(minx, maxx),
     add = ifelse(separate, FALSE, TRUE)
   )
 
   # add legend if the plot is combined
   if (!separate) {
-    legend("topright",
+    graphics::legend("topright",
       legend = c(paste0("Mean: ", meanx1), paste0("Mean: ", meanx2), "Overlap"),
       fill = color
     )
--- R/seq2.R
+++ R/seq2.R
@@ -1,10 +1,28 @@
-#' Variation of seq
+#' Increment a number till a length is reached
 #'
-#' increment a number till you get count
+#' Designed to increment a number by a specified value
+#' until a predefined count is reached.
+#'
+#' @param start starting count
+#' @param increment number to increment by
+#' @param count length of output
+#'
+#' @return numbers dictated by increment number
+#'
+#' @details
+#' Users can easily leverage this function to automate the incrementation process,
+#' making it a convenient solution for repetitive tasks. It is just a surrogate to the
+#' seq() function, more useful depending on the goal of the user.
 #'
 #' @examples
-#' #ex1
-#' seq3(2,10,n=5)
+#' # task 1: starting with 2, increment by 10 until 5 numbers are obtained
+#' seq3(2,10,count=5)
+#'
+#' # task 2: starting with 4, increment by 4 until 4 numbers are archieved
+#' # this is similar (but simpler) to obtaining 4 multiples of 4
+#' # this may also be achieved with seq function like
+#' # seq(4,16,4) or seq(4,16,length.out=4)
+#' seq3(4,4,4)
 #'
 #' @export
 #'
