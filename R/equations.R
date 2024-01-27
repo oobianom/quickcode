51aa7849d74a386e7773c2f03db41d0bf6c56fcc
Obi Obianom <oobianom@umaryland.edu>
2024-01-26 22:57:04 -0500

    Update equations set 1

--- R/equations.R
+++ R/equations.R
@@ -34,3 +34,70 @@ geo.cv <- function(num, round = 2, na.rm = TRUE, neg.rm = TRUE, pct = TRUE) {
   round(res,round)
 }
 
+
+
+#' Calculate geometric mean and round
+#'
+#' Calculate the geometric mean
+#'
+#' @rdname geostats
+#' @param num vector of numbers
+#' @param na.rm remove NAs from the vector
+#' @param neg.rm remove negative values from the vector
+#' @param round round result to decimal place
+#' @return the geometric mean of a set of numbers
+#' @examples
+#' num1 <- sample(300:3000,10)
+#'
+#' #get the geometric mean, excluding all negatives and round to 2
+#' geo.mean(num1)
+#'
+#' #or
+#' geo.mean(num1)
+#'
+#'
+#' #get geometric mean, but round the final value to 5 decimal places
+#' geo.mean(num1, round = 5)
+#'
+#' @export
+
+geo.mean <- function(num, round = 2, na.rm = TRUE, neg.rm = TRUE) {
+  if(not.numeric(num)) stop("The vector must have numbers only")
+  if(neg.rm) num <- num[num > 0]
+  return(round(exp(base::sum(log(num), na.rm = na.rm) / length(num)),round))
+}
+
+
+#' Calculate geometric standard deviation and round
+#'
+#' Calculate the geometric standard deviation
+#'
+#' @rdname geostats
+#' @param num vector of numbers
+#' @param na.rm remove NAs from the vector
+#' @param neg.rm remove negative values from the vector
+#' @param round round result to decimal place
+#' @return the geometric standard deviation of a set of numbers
+#' @examples
+#' num1 <- sample(330:400,20)
+#'
+#' #get geometric SD remove negative values and round to 2 decimal places
+#' geo.sd(num1)
+#'
+#' #get geometric SD, DON'T remove negative values and round to 2 decimal places
+#' geo.sd(num1,na.rm=FALSE)
+#'
+#' #get geometric SD, remove negative values and round to 3 decimal places
+#' geo.sd(num1,round = 3)
+#'
+#' @export
+
+geo.sd <- function(num, round = 2, na.rm = TRUE, neg.rm = TRUE) {
+  if(not.numeric(num)) stop("The vector must have numbers only")
+  if(neg.rm) num <- num[num > 0]
+  return(round(exp(stats::sd(log(num), na.rm = na.rm)),round))
+}
+
+
+
+
