7330501707d646025050e8732addb03cae4f8d6c
Obi Obianom <oobianom@umaryland.edu>
2024-02-06 01:47:49 -0500

    Update 0.7_combo_func1.R

--- R/0.7_combo_func1.R
+++ R/0.7_combo_func1.R
@@ -1,3 +1,11 @@
 # Combinatory functions
-
+#' Split a string of numbers and return numeric
+#'
+#' Given a sting, split by a separator into numbers
+#'
+#' @inherit  base strsplit
 #' @export
+
+strsplit.num <- function(x, split, fixed = FALSE, perl = FALSE, useBytes = FALSE){
+  as.numeric(unlist(strsplit(x, split, fixed, perl, useBytes)))
+}
