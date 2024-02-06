f913ef7cdbe33f0412d87182bb9226cede5361a3
Obi Obianom <oobianom@umaryland.edu>
2024-02-06 02:34:32 -0500

    Update 0.7_combo_func1.R

--- R/0.7_combo_func1.R
+++ R/0.7_combo_func1.R
@@ -26,3 +26,52 @@ strsplit.num <- function(x, split, fixed = FALSE, perl = FALSE, useBytes = FALSE
 strsplit.bool <- function(x, split, fixed = FALSE, perl = FALSE, useBytes = FALSE,type = 2){
   as.boolean(unlist(strsplit(x, split, fixed, perl, useBytes)),type = type)
 }
+
+
+
+
+#' Read in a CSV and print first X rows and Columns
+#'
+#' Read a dataset of type csv and print x rows and y columns
+#'
+#' @inheritParams read.csv
+#' @param type type of return, see the as.boolean function for more info
+#' @return read csv content
+#' @export
+
+read.csv.print <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
+                           fill = TRUE, comment.char = "", ...,dim = c(10L,5L)){
+  x <- read.table.print(file = file, header = header, sep = sep, quote = quote,
+                  dec = dec, fill = fill, comment.char = comment.char, dim = 0, ...)
+  if(multiply(dim) > 0) print(x, dim)
+  x
+}
+
+#' Read in a CSV and print first X rows and Columns
+#'
+#' Read a dataset of type csv and print x rows and y columns
+#'
+#' @inheritParams read.csv
+#' @param type type of return, see the as.boolean function for more info
+#' @return read table content
+#' @export
+
+read.table.print <- function(x, split, fixed = FALSE, perl = FALSE, useBytes = FALSE,type = 2){
+  x <- read.table(file = file, header = header, sep = sep, quote = quote,
+                        dec = dec, fill = fill, comment.char = comment.char, dim = c(10,10), ...)
+  if(n > 0) print(x, dim)
+  x
+}
+
+
+
+#' Multiple a vector of numeric values
+#'
+#' Mulitple all the content of a vector
+#'
+#' @param ... the numeric values to multiply
+#' @return multiple of all content
+#' @export
+
+multiply <- function(...)tail(cumprod(unlist(...)),n=1)
+
