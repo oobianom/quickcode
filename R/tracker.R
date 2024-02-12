7f53ca36e586c78cd41472430b003ef45272ed8b
Obi Obianom <oobianom@umaryland.edu>
2024-02-12 01:28:20 -0500

    Update tracker.R

--- R/tracker.R
+++ R/tracker.R
@@ -16,5 +16,43 @@
 # type1(number(10))
 # type3(number(12))
 
+#' @export
 
+newSuperVar <- function(..., value = 1, class = character, lock = TRUE){
+  message("This function is still being developed")
+  .v <- as.list(substitute(args(...))[-1L])
+  .spkg = new.env()
+  if(!is.attached(super.)){
+    attach(list(), name = super.)
+  }
+  super.env <- as.environment(super.)
 
+  #i1 <- paste0(i,".",c("rm","set","reclass","sort","subset"))
+
+  for(i in .v){
+    assign(paste0(i),value, envir = super.env)
+    lockBinding(paste0(i), env = super.env)
+    assign(paste0(i,".rm"),eval(parse(text=paste0("function() rm(list = '",i,"', envir = super.env)"))) , envir = super.env)
+    assign(paste0(i,".set"),eval(parse(text=paste0("function(b) assign('",i,"', b, envir = super.env)"))) , envir = super.env)
+    assign(paste0(i,".contains"),eval(parse(text=paste0("function(b) assign('",i,"', b, envir = super.env)"))) , envir = super.env)
+    assign(paste0(i,".reclass"),eval(expression({
+      function(b) assign(get(i), b + 1, envir = super.env)
+      })), envir = super.env)
+
+    #attach(.spkg, name = super.)
+
+  }
+
+  lockEnvironment(super.)
+
+  #.. <- substitute(class)
+  # print(..)
+  # .spkg = new.env()
+  # #.spkg[['mutate']] = dplyr::mutate
+  # if(exists(paste0(as.character(..),".add"),envir = as.environment(paste0("package:",.packageName,""))))
+  #   return(assign(paste0(as.character(..),".add"),898899,envir = as.environment(paste0("package:",.packageName,"_superVar"))))
+  # assign(paste0(..,".add"),function(b) get(as.character(..)) + b, envir = .spkg)
+
+}
+
+#lockBinding("yooo", env = env1)
