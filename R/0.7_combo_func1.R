734ea171c53a9a11ecdba1c3202a3d43d2f3331c
Obi Obianom <oobianom@umaryland.edu>
2024-03-03 01:34:44 -0500

    Update 0.7_combo_func1.R

--- R/0.7_combo_func1.R
+++ R/0.7_combo_func1.R
@@ -1,6 +1,6 @@
 # Functions that generally go together often
 
-#' Split a string of numbers and return numeric
+#' Split a string of numbers and return as numeric vector
 #'
 #' Given a sting, split by a separator into numbers
 #'
@@ -11,8 +11,26 @@
 #' @param fixed logical. If TRUE match split exactly, otherwise use regular expressions. Has priority over perl.
 #' @return numeric values based on split string
 #'
+#' @description
+#' The purpose of this function is combine the functionality of
+#' \strong{strsplit}, \strong{unlist} and \strong{as.numeric}, which are often used together.\cr\cr
+#'
 #' @examples
-#' # to be added
+#' # Example 1
+#' # string of numbers with separator " "
+#' num.01 = "5 3 2 3 5 2 33 23 5 32 432 42 23 554"
+#'
+#' # split a string of numbers and return as numeric
+#' strsplit.num(num.01, split = " ")
+#'
+#'
+#' # Example 2
+#' # string of numbers with separator "|||"
+#' num.02 = "0|||1|||4|||43|||6|||8|||00||| 1||| 0 1 T |||F TRUE |||f"
+#'
+#' # split a string of numbers and return as numeric
+#' strsplit.num(num.02, split = "|||")
+#'
 #' @export
 
 strsplit.num <-
@@ -26,7 +44,7 @@ strsplit.num <-
 
 
 
-#' Split a string of values and return as boolean
+#' Split a string of values and return as boolean vector
 #'
 #' Given a sting, split by a separator into boolean
 #'
@@ -38,9 +56,36 @@ strsplit.num <-
 #' @param type type of return, see the as.boolean function for more info
 #' @return boolean values based on split string
 #'
+#' @description
+#' The purpose of this function is combine the functionality of
+#' \strong{strsplit}, \strong{unlist} and \strong{as.logical}, which are often used together.\cr\cr
 #'
 #' @examples
-#' # to be added
+#' # string of numbers
+#' num.01 = "0 1 0 0 1 0 1 T F TRUE FALSE t f"
+#'
+#' # split a string of numbers and return as boolean 1/0
+#' strsplit.bool(num.01, split = " ", type = 3)
+#'
+#' # split a string of numbers and return as boolean TRUE/FALSE
+#' strsplit.bool(num.01, split = " ", type = 2)
+#'
+#' # split a string of numbers and return as boolean Yes/No
+#' strsplit.bool(num.01, split = " ", type = 1)
+#'
+#'
+#' # string of numbers
+#' num.02 = "0abc1abc0abc0abc1abc0abc1abcTabcFabcTRUEabcFALSEabcf"
+#'
+#' # split a string of numbers and return as boolean 1/0
+#' strsplit.bool(num.02, split = "abc", type = 3)
+#'
+#' # split a string of numbers and return as boolean TRUE/FALSE
+#' strsplit.bool(num.02, split = " ", type = 2)
+#'
+#' # split a string of numbers and return as boolean Yes/No
+#' strsplit.bool(num.02, split = " ", type = 1)
+#'
 #' @export
 
 strsplit.bool <-
@@ -56,17 +101,43 @@ strsplit.bool <-
 
 
 
-#' Read in a CSV and show first X rows and Columns
+#' Read a CSV and preview first X rows and columns
 #'
-#' Read a dataset of type csv and show x rows and y columns
+#' Read a dataset of type csv and show x rows and y columns with
+#' one function call
 #'
 #' @param dim dimension of CSV content to show
 #' @inheritParams utils::read.csv
 #' @importFrom utils read.csv
-#' @return read csv content
+#' @return read csv content and a print out of the data head
+#'
+#' @description
+#' The purpose of this function is combine the functionality of
+#' \strong{read.csv} and \strong{print}, which are often used together.\cr\cr
+#' The purpose of this function is to read data from a file into a variable and
+#' simultaneously display a preview of the data, showing either the first few rows or
+#' columns based on the user's specification. It is important to emphasize that the
+#' function expects the user to assign the result of the read operation to a variable
+#' in order to achieve its intended purpose. eg. Use \strong{var1 = read.csv.print(file1)} instead of
+#' \strong{read.csv.print(file1)}
+#'
 #'
 #' @examples
-#' # to be added
+#' \dontrun{
+#' # Example: read a csv file and print the first 10 lines
+#' # declare file
+#' new.file <- "test.csv"
+#'
+#' # read file and preview default
+#' dth3 <- read.csv.print(file = new.file)
+#'
+#' # read file and preview 10 rows and all columns
+#' dth1 <- read.csv.print(file = new.file, dim = 10)
+#'
+#' # read file and preview 10 rows and 5 columns
+#' dth2 <- read.csv.print(file = new.file, dim = c(10,5))
+#' }
+#'
 #' @export
 
 read.csv.print <-
@@ -100,17 +171,42 @@ read.csv.print <-
     x
   }
 
-#' Read in a CSV and show first X rows and Columns
+#' Read in a table and show first X rows and columns
 #'
-#' Read a dataset of type csv and show x rows and y columns
+#' Read a dataset of type table and show x rows and y columns
 #'
 #' @param dim dimension of table content to show
 #' @inheritParams utils::read.table
 #' @importFrom utils read.table
-#' @return read table content
+#' @return read table content and a print out of the data head
+#'
+#' @description
+#' The purpose of this function is combine the functionality of
+#' \strong{read.table} and \strong{print}, which are often used together.\cr\cr
+#' The purpose of this function is to read table from a file into a variable and
+#' simultaneously display a preview of the data, showing either the first few rows or
+#' columns based on the user's specification. It is important to emphasize that the
+#' function expects the user to assign the result of the read operation to a variable
+#' in order to achieve its intended purpose. eg. Use \strong{var1 = read.table.print(file1)} instead of
+#' \strong{read.table.print(file1)}
+#'
 #'
 #' @examples
-#' # to be added
+#' \dontrun{
+#' # Example: read a table file and print the first 10 lines
+#' # declare file
+#' new.file <- "test.tab"
+#'
+#' # read file and preview default
+#' dth3 <- read.table.print(file = new.file, sep=";")
+#'
+#' # read file and preview 10 rows and all columns
+#' dth1 <- read.table.print(file = new.file, sep=";", dim = 10)
+#'
+#' # read file and preview 10 rows and 5 columns
+#' dth2 <- read.table.print(file = new.file, sep=";", dim = c(10,5))
+#' }
+#'
 #' @export
 
 read.table.print <-
