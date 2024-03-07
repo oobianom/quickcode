c1f024a97fbf7347aa9e562d9a597c36401765f7
Obi Obianom <oobianom@umaryland.edu>
2024-03-07 05:01:19 -0500

    Update 0.8.R

--- R/0.8.R
+++ R/0.8.R
@@ -1,9 +1,20 @@
-Date1to3 <- function(x){
-
+Date1to3 <- function(data){
+  if (class(data) != "Date") { stop("class(data) is not an object of class Date")
+  }
+  str = as.character(data)
+  yr1 = easyr::left(str, 4)
+  mth1 = easyr::mid(str, 6, 2)
+  day1 = easyr::right(str, 2)
+  x = data.frame(yr1, mth1, day1)
+  x
 }
 Date3to1 <- function(...){
 
 }
-switch_rows <- function(data,row1,row2,keep){
 
+#' @export
+switch_rows <- function(data,row1,row2,keep){
+  .x2 <- data[row2,]
+  data[row2,] <- data[row1,]
+  data[row1,] <- data[row2,]
 }
