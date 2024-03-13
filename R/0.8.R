79c0e41438a8e34b0b65a6231595ff091aeefb25
Obi Obianom <oobianom@umaryland.edu>
2024-03-13 14:26:24 -0400

    Update 0.8.R

--- R/0.8.R
+++ R/0.8.R
@@ -49,3 +49,62 @@ fun.time <- function(...){
   local(...)
   Sys.time() - .m
 }
+
+#https://www.stat.umn.edu/geyer/old/5101/rlook.html
+
+# Distribution	Functions
+# Beta	pbeta	qbeta	dbeta	rbeta
+# Binomial	pbinom	qbinom	dbinom	rbinom
+# Cauchy	pcauchy	qcauchy	dcauchy	rcauchy
+# Chi-Square	pchisq	qchisq	dchisq	rchisq
+# Exponential	pexp	qexp	dexp	rexp
+# F	pf	qf	df	rf
+# Gamma	pgamma	qgamma	dgamma	rgamma
+# Geometric	pgeom	qgeom	dgeom	rgeom
+# Hypergeometric	phyper	qhyper	dhyper	rhyper
+# Logistic	plogis	qlogis	dlogis	rlogis
+# Log Normal	plnorm	qlnorm	dlnorm	rlnorm
+# Negative Binomial	pnbinom	qnbinom	dnbinom	rnbinom
+# Normal	pnorm	qnorm	dnorm	rnorm
+# Poisson	ppois	qpois	dpois	rpois
+# Student t	pt	qt	dt	rt
+# Studentized Range	ptukey	qtukey	dtukey	rtukey
+# Uniform	punif	qunif	dunif	runif
+# Weibull	pweibull	qweibull	dweibull	rweibull
+# Wilcoxon Rank Sum Statistic	pwilcox	qwilcox	dwilcox	rwilcox
+# Wilcoxon Signed Rank Statistic	psignrank	qsignrank	dsignrank	rsignrank
+
+
+#check if data is log normal
+#' Check if a vector of values are Normal or Lognormal distributed
+#'
+#' @description
+#' Check whether a vector of data contains values that fit a lognormal or normal distribution
+#' @details
+#' This function takes a numeric vector as its input. This vector contains the dataset that will be analyzed.
+#' \cr
+#' This function first performs a Shapiro-Wilk test on the data to check if it comes from a normal distribution. The Shapiro-Wilk test checks if sample data came from a normal distribution. It returns a p-value, with a higher p-value indicating stronger evidence that the data is normally distributed.
+#' \cr
+#' If the p-value from the Shapiro-Wilk test is above a predefined threshold (such as 0.05), the data is considered normally distributed. In this case, the function returns "normal".
+#' \cr
+#' If the data fails the Shapiro-Wilk normality test, the function then checks if the data fits a lognormal distribution. It transforms the data by taking the natural logarithm of each value. It then runs the Shapiro-Wilk test on the transformed data.
+#' \cr
+#' If the p-value from this test on the logged data is above the threshold, the function returns "lognormal", indicating the untransformed data fits a lognormal distribution after logging.
+#' \cr
+#' If the data fails both the normal and logged Shapiro-Wilk tests, the function returns "neither", indicating the data does not appear to come from a normal or lognormal distribution based on the tests.
+#'
+#' @rdname distribution_check
+#' @param values vector of values
+#' @return boolean value if lognormal distributed
+#' @export
+is.lognormal <- function(values){
+  (stats::shapiro.test(log(values)))$p.value >= 0.05
+}
+
+#' @rdname distribution_check
+#' @param values vector of values
+#' @return boolean value if normal distributed
+#' @export
+is.normal <- function(values){
+  (stats::shapiro.test(values))$p.value >= 0.05
+}
