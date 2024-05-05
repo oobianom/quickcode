510aab698c4efc3d78cd3fc3d8745726e04f4007
Obi Obianom <oobianom@umaryland.edu>
2024-05-05 13:44:07 -0400

    Create dat2week.R

---
+++ R/dat2week.R
@@ -0,0 +1,24 @@
+#' Convert Date to Numeric Week
+#'
+#' Convert the input date to week
+#'
+#' @export
+#'
+
+getWeekSeq <- function(start_date, end_date) {
+  # Convert input dates to Date objects
+  start_date <- as.Date(start_date)
+  end_date <- as.Date(end_date)
+
+  # Generate a sequence of dates
+  date_sequence <- seq(from = start_date, to = end_date, by = "day")
+
+  # Create a data frame
+  week_df <- data.frame(DATE = date_sequence)
+
+  # Add a column for the week's sequence starting with the numeric week of the year as provided by lubridate's week function
+  sequence = lubridate::week(week_df$DATE)
+  week_df$WEEK <- paste("WEEK", sequence)
+
+  return(week_df)
+}
