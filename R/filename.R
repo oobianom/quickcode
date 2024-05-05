c11371164df8d04956e2b77c18356d2feedbfd7a
Obi Obianom <oobianom@umaryland.edu>
2024-05-05 13:17:24 -0400

    Update filename.R

--- R/filename.R
+++ R/filename.R
@@ -26,12 +26,20 @@
 #' fAddDate("one_file_name_fileend.pdf")
 #'
 #' # Task 4
-#' fAddDate(c("path1/","path2/"),"filepre","filemid",c("fileend.png",".pdf"))
+#' fAddDate(c("path1/","path2/"),"file1-","filemid",c("fileend.png",".pdf"))
 #'
 #' # Task 5
 #' data("USArrests")
 #' USArrests$fn = paste0(row.names(USArrests), ".txt")
 #' head(fAddDate(USArrests$fn),10)
+#'
+#' # Task 6: format date - month.day.year
+#' fAddDate("sample_file_name.pdf", format = "%B.%d.%YYYY")
+#'
+#' # Task 7: format date - year_month
+#' fAddDate("sample_file_name.pdf", format = "%YYYY_%m")
+#'
+#'
 #' @export
 
 fAddDate <- function(...,format = "%d-%b-%Y"){
