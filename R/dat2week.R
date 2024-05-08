e1f588e2c673384e13b8462ef36c0a39485bacb2
Obi Obianom <oobianom@umaryland.edu>
2024-05-08 15:43:16 -0400

    Update dat2week.R

--- R/dat2week.R
+++ R/dat2week.R
@@ -1,14 +1,21 @@
-#' Derive Numeric Week Based on Dates
+#' Convert Dates into Numeric Week Counts
 #'
 #' Convert the range of date to number of weeks
 #'
-#' @param start_date the start date
-#' @param end_date the end date
+#' @param date A variable of dates that need not be sequential but that reference values of class Date; if this argument is populated, the start_date and end_date arguments must be empty
+#' @param start_date A scaler of class Date (if this argument is populated, the date arg must be empty)
+#' @param end_date A scaler of class Date; must be later than the start_date (if this argument is populated, the date arg must be empty)
+#' @param seq = The sequential method used to generate the data frame; the options for configuring this argument are as follows:
+#' \cr\cr - bywk
+#' \cr\cr - byseq
+#' \cr\cr - bycont
+#' @note
+#' The default configuration for the seq argument should be "bywk."
 #'
 #' @export
 #'
 
-getWeekSeq <- function(start_date, end_date) {
+getWeekSeq <- function(start_date, end_date, date, seq) {
   # Convert input dates to Date objects
   start_date <- as.Date(start_date)
   end_date <- as.Date(end_date)
