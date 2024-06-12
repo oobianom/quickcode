d6ab5d90af41c2b577755eff8cb433570e7d537f
Obi Obianom <oobianom@umaryland.edu>
2024-06-12 05:14:50 -0400

    Update sort.R

--- R/sort.R
+++ R/sort.R
@@ -1,7 +1,29 @@
+#' Sort vector by length of its content
+#'
+#' Sort the length of the content of a vector
+#'
+#' @param vec a vector
+#' @param asc TRUE or FALSE whether to sort by ascending or descending order
+#' @note
+#' This function removes all NAs prior to sorting the vector
+#' @examples
+#' # sort by length of content
+#' x = c("acs","tt","jdssr","h","grab")
+#' y = sort.length(vec = x)
+#' z = sort.length(vec = x, asc = FALSE)
+#'
+#'
 #' @export
 #'
 
-lsort <- function(i,order = c("asc","desc")){
-  message("still under development")
-  i1 <- nchar(i)
+sort.length <- function(vec, asc = TRUE) {
+  # Remove NA values
+  vec <- vec[not.na(vec)]
+
+  # Compute lengths of elements
+  lengths <- nchar(vec)
+
+  # Sort indices based on lengths
+  # Return sorted vector
+  vec[order(lengths, decreasing = !asc)]
 }
