da1a04bc7a9c926b8eedb9119ad42ec12c5fffd9
Obi Obianom <oobianom@umaryland.edu>
2024-06-15 15:37:23 -0400

    Create getdist.R

---
+++ R/getdist.R
@@ -0,0 +1,24 @@
+#' Get distribution of a data
+#'
+#' Check a data against multiple distributions
+#' @param data the data to check against distributions
+#' @param alpha p-value alpha level
+#' @export
+#'
+
+getDistribution <- function(data, alpha = 0.05) {
+  setDisAlpha(alpha = alpha)
+  dists <- list(
+    lognormal = is.lognormal(data, alpha = alpha, method = 1),
+    normal = is.normal(data, alpha = alpha, method = 1),
+    uniform = is.uniform(data, alpha = alpha),
+    poisson = is.poisson(data, alpha = alpha),
+    gamma = is.gamma(data, alpha = alpha),
+    logistic = is.logistic(data, alpha = alpha),
+    weibull = is.weibull(data, alpha = alpha),
+    cauchy = is.cauchy(data, alpha = alpha)
+  )
+  unsetDisAlpha()
+
+  dists
+}
