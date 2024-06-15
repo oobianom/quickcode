b295b39187609c7c6f87e8f7816d3bf82ea7a8e1
Obi Obianom <oobianom@umaryland.edu>
2024-06-15 15:30:48 -0400

    Update mutate_filter.R

--- R/mutate_filter.R
+++ R/mutate_filter.R
@@ -2,6 +2,14 @@
 #'
 #' @export
 #'
-mutate_filter <- function(){
+mutate_filter <- function(.,filter,...){
+  .dt <- .
+  .dt$refonumval__1 <- 1:nrow(.dt)
+  .dt1 <- .dt
+  data_pop_filter(.dt1,remove = filter)
+  .mut <- as.character(list(substitute(...)))
+  .dt2 <- with(subset(.dt,filter),{
+    eval(parse(text = .mut))
+  })
 
 }
