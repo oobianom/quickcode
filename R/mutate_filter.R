77742fc4658ca826e3fa36b8e70a52aeb25cb79e
Obi Obianom <oobianom@umaryland.edu>
2024-06-19 16:23:25 -0400

    Update mutate_filter.R

--- R/mutate_filter.R
+++ R/mutate_filter.R
@@ -1,15 +1,17 @@
 #' Mutate a subset of dataset, and reattach it to the dataset
-#'
+#' @param filter subset of data to modify
+#' @param ... mutation syntax similar to dplyr::mutate
 #' @export
 #'
-mutate_filter <- function(.,filter,...){
-  .dt <- .
-  .dt$refonumval__1 <- 1:nrow(.dt)
-  .dt1 <- .dt
-  data_pop_filter(.dt1,remove = filter)
-  .mut <- as.character(list(substitute(...)))
-  .dt2 <- with(subset(.dt,filter),{
-    eval(parse(text = .mut))
-  })
 
+mutate_filter <- function(., filter., ...) {
+  .dt <- .
+  .dt$refonumval__91 <- 1:nrow(.dt)
+  .half1 <- .dt
+  .fi <- as.character(list(substitute(filter.)))
+  eval(parse(text = paste0(".half2 = subset(.half1,", .fi, ");.half1 = subset(.half1,!(", .fi, "))")))
+  .half2 <- dplyr::mutate(.half2, ...)
+  .full <- rbind(.half1, .half2)
+  .full[order(.full$refonumval__91), names(.full) %nin% c("refonumval__91")]
 }
+
