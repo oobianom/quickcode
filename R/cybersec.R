b7504e000ba813758f89f80e86706ba9b72a5b89
Obi Obianom <oobianom@umaryland.edu>
2024-06-29 21:29:21 -0400

    Update cybersec.R

--- R/cybersec.R
+++ R/cybersec.R
@@ -3,12 +3,12 @@
 #' Get all active connections
 #'
 #' @export
-#' @return a data frame containing the list of all connextions
+#' @return a data frame containing the list of all connections
 #'
 #' @examples
 #'
-#' # Get all connections on my system
-#' x = get_active_conn()
+#' # Get a list of all connections on my system
+#' get_active_conn()
 #'
 #'
 #' @rdname cybersecurity
@@ -39,7 +39,48 @@ get_active_conn <- function(){
     colnames(df) <- c("Proto", "Local_Address", "Foreign_Address", "State", "PID")
 
     # Reset dataframe
-    df <- df[-c(1),]
-
-    return(df)
+    df[-c(1),]
   }
+
+
+#' @rdname cybersecurity
+#' @param input input string containing IP
+#' @return extract_IP returns a vector of IP addresses
+#'
+#' @examples
+#' # Extract all IP addresses from a string
+#'
+#' # Example 1
+#' x = system("ipconfig /all", intern = TRUE)
+#' extract_IP(x)
+#'
+#' # Example 2
+#' # This example demonstrates the separate
+#' # extraction of an IP address one per vector element:
+#' str1 = c("Two IP(66.544.33.54) addresses
+#' were discovered in the scan.
+#' One was at 92.234.1.0.",
+#' "The other IP was 62.3.45.255.")
+#' extract_IP(str1)
+#'
+#' # Example 3:
+#' # This example demonstrates the extraction
+#' # of multiple IP addresses from a single vector element:
+#' str2 = "Two IP addresses were discovered
+#' in the scan. One was at 92.234.1.0.
+#' The other IP was 62.3.45.255."
+#' extract_IP(str2)
+#'
+#' @export
+
+extract_IP <- function(input) {
+  # Match an IP address pattern
+  ip_pattern <- "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
+
+  # Extract all occurrences of IP addresses
+  extracted_ips <- stringr::str_extract_all(input, ip_pattern)
+
+  # Deconstruct the list object
+  # Return the extracted IP addresses (or an empty list if not found)
+  unlist(extracted_ips)
+}
