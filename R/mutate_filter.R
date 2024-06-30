b6af6cc3c31ce4f4d15bfef1a6091f5dece13e93
Obi Obianom <oobianom@umaryland.edu>
2024-06-29 21:29:34 -0400

    Update mutate_filter.R

--- R/mutate_filter.R
+++ R/mutate_filter.R
@@ -1,30 +1,36 @@
-#' Mutate a subset of dataset, and reattach it to the dataset
+#' Mutate only a subset of dataset intact
 #'
 #' Extension of the dplyr::mutate function that allows the user to mutate only a specific filtered subset of a data, while leaving the other parts of the data intact
 #' @return data frame containing original data, but with a subset mutated
+#' @param . data object
 #' @param subset subset of data to modify
 #' @param ... mutation syntax similar to dplyr::mutate
 #'
 #' @examples
 #' #mutate a subsection filter of mtcars
-#' dt =mtcars
+#' dt = mtcars
+#' names(dt)
+#' head(dt)
 #' mutate_filter(dt,mpg == 21.0 & cyl == 6, cyl=1000,hp=2000,vs=hp*2)
+#'
+#'
+#' dt2 = beaver1
+#' names(dt2)
+#' head(dt2)
+#' mutate_filter(dt2, day == 346 & time < 1200, activ = 12, temp = round(temp*10,1))
 #' @export
 #'
 
 mutate_filter <- function(., subset, ...) {
-  .dt <- .
-  #.dt$refonumval__92 <- 0
-  .dt$refonumval__91 <- 1:nrow(.dt)
-  .half1 <- .dt
+  stopifnot(inherits(.,"data.frame"))
+  .half1 <- .
+  .half1$refonumval__91 <- 1:nrow(.half1)
   .fi <- as.character(list(substitute(subset)))
   eval(parse(text = paste0(".half2 = subset(.half1,", .fi, ");.half1 = subset(.half1,!(", .fi, "))")))
   .half2 <- transform(.half2,...)
   .full <- rbind(.half1, .half2[,names(.half1)])
-  .full[order(.full$refonumval__91), names(.full) %nin% c("refonumval__91", "refonumval__92")]
+  .full[order(.full$refonumval__91), names(.full) %nin% "refonumval__91"]
 }
 
-#dt = mtcars
-#mutate_filter(dt,mpg == 21.0 & cyl == 6, cyl=1000,hp=2000,vs=hp*2)
 
 
