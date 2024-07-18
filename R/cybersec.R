31f47cfac0a2206c72e2365581dfc54a4f1cc5dd
Obi Obianom <oobianom@umaryland.edu>
2024-07-18 14:03:39 -0400

    Update cybersec.R

--- R/cybersec.R
+++ R/cybersec.R
@@ -97,18 +97,27 @@
 #'
 #'
 #' @rdname cybersecurity
+#' @export
 #'
-# getMyIP <- function() {
-#   # Get vector element of machine's local IP address
-#   loc <- grep("IPv4", system("ipconfig", intern = TRUE))
-#   ip <- system("ipconfig",intern = TRUE)[loc]
-#
-#   # Match an IP address pattern
-#   ip_pattern <- "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
-#
-#   # Extract the occurrence of an IP address
-#   # Return the extracted IP address (or character(0) if not found)
-#   regmatches(ip, regexpr(ip_pattern, ip))
-# }
+getMyIP <- function() {
+  # Match an IP address pattern
+  ip_pattern <- "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
+  if(Sys.info()['sysname'] == "Linux"){
+    loc <- grep("eth0$", system("ip -4 a", intern = TRUE))
+    ip <- system("ip -4 a",intern = TRUE)[loc]
+    # Extract the occurrence of an IP address
+    # Return the extracted IP address (or character(0) if not found)
+    regmatches(ip, regexpr(ip_pattern, ip))
+  }
+  if(Sys.info()['sysname'] == "Windows"){
+    # Get vector element of machine's local IP address
+    loc <- grep("IPv4", system("ipconfig", intern = TRUE))
+    ip <- system("ipconfig",intern = TRUE)[loc]
+
+    # Extract the occurrence of an IP address
+    # Return the extracted IP address (or character(0) if not found)
+    regmatches(ip, regexpr(ip_pattern, ip))
+  }
+}
 
 
