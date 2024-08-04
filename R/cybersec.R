f4a023142f8929081caaa9fa8694ffa97a1f5c2d
Obi Obianom <oobianom@umaryland.edu>
2024-08-04 13:50:29 -0400

    Update cybersec.R

--- R/cybersec.R
+++ R/cybersec.R
@@ -1,4 +1,41 @@
+#' @rdname cybersecurity
+#' @param input input string containing IP
+#' @return extract_IP returns a vector of IP addresses
+#'
+#' @examples
+#' \donttest{
+#' # Extract all IP addresses from a string
+#'
+#' # Example 1
+#' # This example demonstrates the separate
+#' # extraction of an IP address one per vector element:
+#' str1 = c("Two IP(66.544.33.54) addresses
+#' were discovered in the scan.
+#' One was at 92.234.1.0.",
+#' "The other IP was 62.3.45.255.")
+#' extract_IP(str1)
+#'
+#' # Example 2
+#' # This example demonstrates the extraction
+#' # of multiple IP addresses from a single vector element:
+#' str2 = "Two IP addresses were discovered
+#' in the scan. One was at 92.234.1.0.
+#' The other IP was 62.3.45.255."
+#' extract_IP(str2)
+#' }
+#'
 
+extract_IP <- function(input) {
+  # Match an IP address pattern
+  ip_pattern <- "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
+
+  # Extract all occurrences of IP addresses
+  extracted_ips <- gregexpr(ip_pattern, input)
+
+  # Deconstruct the list object
+  # Return the extracted IP addresses (or an empty list if not found)
+  unlist(regmatches(input, extracted_ips))
+}
 
 #'
 # get_active_conn <- function(){
@@ -23,22 +60,6 @@
 #     # Reset dataframe
 #     df[-c(1),]
 #   }
-
-
-
-
-
-# extract_IP <- function(input) {
-#   # Match an IP address pattern
-#   ip_pattern <- "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b"
-#
-#   # Extract all occurrences of IP addresses
-#   extracted_ips <- gregexpr(ip_pattern, input)
-#
-#   # Deconstruct the list object
-#   # Return the extracted IP addresses (or an empty list if not found)
-#   unlist(regmatches(input, extracted_ips))
-# }
 
 
 
@@ -70,3 +91,5 @@
 # }
 #
 #
+
+
