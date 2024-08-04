dcb678d96bc6a9c0e984a0fcb912b2dc3acfc7af
Obi Obianom <oobianom@umaryland.edu>
2024-08-04 16:42:09 -0400

    Update chain.R

--- chain.R
+++ chain.R
@@ -1,18 +1,39 @@
-#' Function chaining
+#' Simple Function chaining
 #'
 #' Chain multiple function to a call
 #'
+#' @rdname simplechaining
+#' @param obj data object to apply function
+#' @param funcs function chains to apply to the data object
+#' @param sep separated for funcs argument values
+#' @return the result of applying the chained functions to the data object
+#' @note
+#' chain_sep allows the user to preset the separator for the function chaining \cr\cr
+#' e.g. you can call the function to set sep = "__" before using the %.% call to parse the chained function "__unique__sum"
 #' @examples
-#' 1:3%.%.unique.length
-#' sample(1:1000,10,replace=TRUE) %.%.unique.length
+#' #use defult sep ".."
+#' 1:3%.%unique.length
+#' sample(1:1000,10,replace=TRUE) %.%unique..length
+#' sample(1:10,10,replace=TRUE) %.%unique..cumsum
+#'
+#' # set sep before function chaining
+#' chain_sep("__")
+#' sample(1:10,10,replace=TRUE) %.%unique__cumsum
+#' sample(1:10,10,replace=TRUE) %.%unique__cumsum__length
 #' @export
 
-`%.%` <- function(left, right){
-  .pF <- as.character(substitute(right))
-  .pF2 <- strsplit(.pF,"\\.")[[1]][-1]
-  lapply(.pF2, function(l) eval(parse(text = paste0("left <<-left %>% ",l))))
-  left
+`%.%` <- function(obj, funcs){
+  sep. <- options()$quickcode_chain_sep %or% "\\.\\."
+  .pF <- as.character(substitute(funcs))
+  .pF2 <- strsplit(.pF,sep.)[[1]]
+  print(.pF2)
+  lapply(.pF2, function(l) eval(parse(text = paste0("obj <<-obj %>% ",l))))
+  obj
 }
 
 
-
+#' @export
+#' @rdname simplechaining
+chain_sep <- function(sep = "\\.\\."){
+  options(quickcode_chain_sep = sep)
+}
