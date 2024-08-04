880b60d97d3cf1f508bde70a1b87985c4a3bed74
Obi Obianom <oobianom@umaryland.edu>
2024-08-04 14:03:33 -0400

    Update randomImage.R

--- R/randomImage.R
+++ R/randomImage.R
@@ -13,15 +13,6 @@
 #' @section Sources & References:
 #' The random images are downloaded from https://picsum.photos
 #'
-#' @section Category Choices:
-#' Categories for 'cat' argument include
-#' "3D", "animals", "architecture", "backgrounds", "beauty", "experimental",
-#' "fashion", "film", "food", "interior", "nature", "people", "renders",
-#' "school", "sports", "travel", "unsplash", "wallpapers".\cr\cr
-#' Image categories can be captured in a separate vector as a cross-reference made available to the cat argument. \cr For example: \cr
-#' imgcat= c("3D", "animals", "architecture", "backgrounds", "beauty", "experimental", "fashion", "film", "food", "interior", "nature", "people", "renders", "school", "sports", "travel", "unsplash", "wallpapers")\cr\cr
-#' genRandImg(fp, cat = imgcat[9], n = 5); Note that cat will be deprecated in version 0.9
-#'
 #'
 #' @section Use case:
 #' This functionality is great for developers trying to obtain one or more images for use in displays/analysis
@@ -47,17 +38,17 @@
 #'
 #' # download 200 random images from category of school
 #' # Note that maximum download is 99, so the function will only download 99
-#' genRandImg(fp = tempdir(),cat = "school", n = 200)
+#' genRandImg(fp = tempdir(), n = 200)
 #'
 #' # download 5 random images with extension jif and return paths
-#' genRandImg(fp = tempdir(),cat = "beauty", n = 5, ext = "jif", paths = TRUE)
+#' genRandImg(fp = tempdir(), n = 5, ext = "jif", paths = TRUE)
 #' }
 #' @export
 #'
 genRandImg = function (fp, n = 1, w.px = 500, h.px = 500,
                        ext = "jpg", paths = FALSE, cat = NULL)
 {
-  if(not.null(cat)) message("Please not that the 'cat' category is now deprecated.")
+  if(not.null(cat)) message("Please note that the 'cat' category is now deprecated.")
   if (!dir.exists(fp))
     stop(paste0("The directory path declared in the 'fp' argument must exist."))
   if (n > 99) {
@@ -86,43 +77,3 @@ genRandImg = function (fp, n = 1, w.px = 500, h.px = 500,
     gsub("\\\\", "/", downloaded.files)
 }
 
-# genRandImg <- function(fp, cat = imageCategories, n = 1, w.px = 500, h.px = 500, ext = "jpg", paths = FALSE) {
-#   # check existence of directory
-#   if(!dir.exists(fp)) stop(paste0("The directory path declared in the 'fp' argument must exist."))
-#
-#   # match category
-#   cat <- match.arg(cat)
-#
-#   # check that n does not exceed 99
-#   if(n > 99){
-#     warning("The value of n exceeds 99, so n was set to 99")
-#     n <- 99L
-#   }
-#   # check sum and file names
-#   checksum.files <- c()
-#   downloaded.files <- c()
-#
-#
-#   #download files to temp directory
-#   while (n > 0) {
-#     file.store <- file.path(fp,paste0(tolower(cat),"_",randString(1,5),n,".",ext))
-#     utils::download.file(
-#       url = paste0("https://source.unsplash.com/random/", w.px, "x", h.px, "/?", cat, "&", n),
-#       destfile = file.store, mode = "wb"
-#     )
-#
-#     # make sure there is no duplication
-#     checksum <- as.vector(tools::md5sum(file.store))
-#     if (checksum %in% checksum.files) {
-#       unlink(file.store)
-#     } else {
-#       vector_push(checksum.files, checksum)
-#       vector_push(downloaded.files, file.store)
-#       minus(n)
-#     }
-#   }
-#   message(paste0("Downloaded ",length(downloaded.files)," files to ",fp))
-#   if(paths) gsub("\\\\","/",downloaded.files)
-# }
-#
-#
