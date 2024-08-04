f8a50b4e83c3856bb51aa3c32fab5476838b3d61
Obi Obianom <oobianom@umaryland.edu>
2024-08-04 00:18:13 -0400

    Update code.R

--- R/code.R
+++ R/code.R
@@ -1,9 +1,19 @@
 #' Wrap code to save space
+#' @examples
+#' c.ode({
+#' y = 7
+#' m = 9
+#' },{
+#'   m= y +9
+#' })
 #'
 #' @export
 
 c.ode <- function(...){
   for(i in 1:...length()){
-    eval(parse(text=...elt()[i]))
+   eval(parse(text=...elt(i)))
   }
 }
+
+
+
