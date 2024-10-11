a463c3a37fb8b42138e873e068c7c27d0954eb18
Obi Obianom <oobianom@umaryland.edu>
2024-10-11 10:31:02 -0400

    Create pkg_by.R

---
+++ R/pkg_by.R
@@ -0,0 +1,47 @@
+#' Fetch R package based on keyword
+#'
+#' This function gets R packages based on a keyword in their description.
+#'
+#' @param keyword character, the keyword to search package descriptions for.
+#'
+#' @return character vector of matching package names or NULL if no matches.
+#'
+#' @note
+#' the function in its current form only searches available.packages()
+#'
+#' @examples
+#' \donttest{
+#' # find the list of R packages for data or machine learning
+#'
+#' matched_pkgs <- find_packages("data")
+#' matched_pkgs
+#'
+#' matched_pkgs <- find_packages("machine learning")
+#' matched_pkgs
+#' }
+#' @export
+
+
+find_packages <- function(keyword) {
+  packages <- rownames(available.packages())
+
+  matched_pkgs <- c()
+
+  for (pkg in packages) {
+    tryCatch({
+      desc <- packageDescription(pkg)$Description
+
+      if (grepl(keyword, desc, ignore.case = TRUE)) {
+        matched_pkgs <- c(matched_pkgs, pkg)
+      }
+    },
+    error = function(e) {
+    })
+  }
+
+  if (length(matched_pkgs) == 0) {
+    return(NULL)
+  } else {
+    return(matched_pkgs)
+  }
+}
