d74e557719023f3bf50f751d764d1be99c928814
Obi Obianom <oobianom@umaryland.edu>
2024-10-14 15:30:54 -0400

    Update data_pop.R

--- R/data_pop.R
+++ R/data_pop.R
@@ -8,6 +8,11 @@
 #' @param ret TRUE or FALSE. whether to return value instead of setting it to the parent data
 #' @return data with elements removed
 #' @examples
+#' #basic example: pop off 1 row from sample data
+#' data.frame(ID=1:10,N=number(10)) #before
+#' data_pop(data.frame(ID=1:10,N=number(10))) #after
+#'
+#' #using data objects
 #' data.01 <- mtcars[1:7,]
 #'
 #' #task: remove 1 element from the end of the data and set it to the data name
@@ -25,11 +30,23 @@
 #' data_pop(data.01,5, ret = TRUE) #return modified data
 #' data.01 #data.01 data remains the same after pop
 #'
+#'
 #' @export
 #'
 data_pop <- function(., n = 1, which = c("rows", "cols"), ret = FALSE) {
   .. <- substitute(.)
   wh <- match.arg(which)
+  if (typeof(..) == "language"){
+    switch(wh,
+           "rows" = {
+             d05 <- .[1:(nrow(.)-n),]
+           },
+           "cols" = {
+             d05 <- .[,1:(ncol(.)-n)]
+           }
+    )
+    return(d05)
+  }
   if (typeof(..) != "symbol") stop(paste0(.., " must be an object."))
   data <- as.data.frame(get(as.character(..), envir = parent.frame()))
   if(nrow(data) & ncol(data)){
