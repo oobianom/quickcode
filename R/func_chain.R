7915b2098578bc12db0c635c339fc05990f08a1a
Obi Obianom <oobianom@umaryland.edu>
2025-01-13 04:16:50 -0500

    Declare chain_func

--- R/func_chain.R
+++ R/func_chain.R
@@ -43,3 +43,64 @@
 chain_sep <- function(sep = "\\.\\."){
   options(quickcode_chain_sep = sep)
 }
+
+
+
+
+
+#' @rdname simplechaining2
+#' Combine specific functions as store as one function
+#' 
+#' Use many functions in one call using predeclared calls
+#' 
+#' @param ... functions to include. see example for how to use
+#' @param otherargs other arguments for use in each function
+#' 
+#' @return returns a function that combines multiple function
+#' @export
+#' @examples
+#' 
+#' # Example 1 with base functions
+#' combf1 <- chain_func(unique, cumsum, print)
+#' result <- combf1(sample(1:5,10,replace = TRUE))
+#' #or 
+#' u = sample(1:5,10,replace = TRUE)
+#' result <- combf1(u)
+#' 
+#' # Example 2 with base functions with arguments
+#' combf2 <- chain_func(unique, print, otherargs = list(.= c(FALSE),.=c(2)))
+#' result <- combf2(sample(1:3,10,replace = TRUE))
+#' 
+#' # Example 3 with custom functions
+#' r = function(a,b,c){
+#'   if(!missing(a))print(a)
+#'   if(!missing(b))print(b)
+#'   if(!missing(c))print(c)
+#'   return(a)
+#' }
+#' 
+#' r2 = function(a,b,c){
+#'   if(!missing(a))message(a)
+#'   if(!missing(b))message(b)
+#'   if(!missing(c))message(c)
+#'   return(a)
+#' }
+#' 
+#' combf3 <- chain_func(r,r2, otherargs =list(.=c("apple","cat"),.=c("rice")))
+#' res <- combf3(head(mtcars))
+
+chain_func <- function(...,otherargs = list()) {
+  # Store the functions in a list
+  functions <- list(...)
+  # Create a function to apply the chained functions
+  return(function(x) {
+    result <- x
+    for (func in indexed(functions)) {
+      list1 = list(result)
+      for (le in unlist(otherargs[func$key])) list1<- c(list1,list(le))
+      result <- do.call(func$value,list1)
+    }
+    return(result)
+  })
+}
+
