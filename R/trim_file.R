2b1d79d66801a4683d2fcce302ecc9f329ee63f2
Obi Obianom <oobianom@umaryland.edu>
2025-02-26 00:04:38 -0500

    Create trim_file.R

---
+++ R/trim_file.R
@@ -0,0 +1,29 @@
+#' Remove Empty Lines from a File
+#'
+#' This function reads a text file, removes all empty lines, and saves the
+#' modified content back to the same file.
+#'
+#' @param file_path A character string specifying the path to the text file.
+#' @param resave A logical value indicating if the file content should be resaved or returned
+#'
+#' @return NULL This function modifies the file in place and does not return a value.
+#'
+#' @examples
+#' # Remove empty lines from a file
+#' remove_empty_lines("path/to/your/file.txt")
+#'
+#' @export
+rm_empty_lines <- function(file_path, resave = TRUE) {
+  if(missing(file_path)) file_path = rstudioapi::getActiveDocumentContext()$path
+
+  # Read the file
+  lines <- readLines(file_path)
+
+  # Remove empty lines
+  non_empty_lines <- lines[nzchar(lines)]
+
+  message(paste0("Empty lines have been removed from ",basename(file_path)))
+
+  # Write the non-empty lines back to the file
+  if(resave) writeLines(non_empty_lines, file_path) else non_empty_lines
+}
